e4692c437264519d71b3edce96fe4c5ca7fdda05
Sontara444 <sontararajput@gmail.com>
2023-08-05 23:29:19 +0530

    add api

---
+++ script.js
@@ -0,0 +1,28 @@
+const accessKey = "Upb8Tjdor5efkPOBBzoVbScs8hjK3G5nW_zHcvszk4E"
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+}
+console.log(data);
+
+
+
+
+
+
+// Upb8Tjdor5efkPOBBzoVbScs8hjK3G5nW_zHcvszk4E
+// 9ziMr2lyhx5sl91jZLjUCcg-RejMzc23wdnHCXbLCtA
+//https:api.unsplash.com/search/photos?page=1&query=office&client_id=Upb8Tjdor5efkPOBBzoVbScs8hjK3G5nW_zHcvszk4E
